5161cc9501d7be07f7879e490a1fdc2d29e27e94
Mariia Stolbtsova <mariashkrabak@gmail.com>
2025-06-24 20:04:47 +0300

    Test first commit

---
+++ script.js
@@ -0,0 +1,83 @@
+"use strict";
+/*
+let number = 5;
+const leftBorderWidth = 1;
+
+number =10;
+console.log(number);
+
+const obj = {
+    a:50
+};
+obj.a = 10;
+console.log(obj);
+
+var name = 'Ivan';
+
+
+lesson 9*/
+/*const vehicleBodyWidth = 5000;
+const vehicleBodyLenght = 4000;
+console.log("Ширина кузова автомобиля: "+ vehicleBodyWidth + ', длина ' + vehicleBodyLenght);
+
+['vruru', 'ttrt', 'fgdg'].map(a =>)
+
+
+//snake_case
+// UPPER_SNAKE_CASE
+//_apiBase
+//Kebab-case
+//PascalCase
+//camelCase
+
+//Lesson 10
+
+ let number = 4;
+ 
+console.log(4/0);
+console.log('string'*9);
+
+const person = 'Alex';
+
+const bool = true;
+
+console.log(something);
+
+let und;
+console.log(und);
+
+const obj = {
+    name: "John",
+    age: 25,
+    isMarried: false
+}
+console.log(obj.name)
+
+let arr=['plum.png', 'orange.jpeg', 'apple.bmp'];
+console.log(arr[1]);
+
+//Lesson 11
+
+const arr2 = [1,2,3];
+const arrObj = {
+    0:1,
+    1:2,
+    2:3
+}
+console.log(arr2[1])
+
+//const obj2 = {a: 1, b: 2};
+
+const obj2 = {Anna: 500,
+    'Alice': 800};
+
+
+    //Lesson 12
+
+    //alert('Hello')*/
+
+   // const result = confirm("Are you here?");
+    //console.log(result)
+
+const answer = prompt("Вам есть 18?", "18");
+console.log(answer)
